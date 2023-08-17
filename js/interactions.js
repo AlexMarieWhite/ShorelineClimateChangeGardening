7da1cbbaa67aa9af736700bceb704a9151e8064f
Alexandra White <138166329+AlexMarieWhite@users.noreply.github.com>
2023-08-16 20:35:55 -0700

    update javascript

---
+++ js/interactions.js
@@ -0,0 +1,16 @@
+document.getElementById("myBtn").onclick = function() {ScrollToTop()};
+window.onscroll = function() {ShowHideBackToTop()};
+
+function ShowHideBackToTop() {
+    let mybutton = document.getElementById("myBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function ScrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
